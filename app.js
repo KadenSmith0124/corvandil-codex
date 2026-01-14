130031559738658d16fda5b03ae4724cf403c8a9
Kaden Smith <kaden.smith0124@gmail.com>
2026-01-14 16:33:39 -0600

    Created a better navbar

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-console.log("AHHHHHHHH");
+//Creates the same Navbar on every page using this script file
+var navbarItems = 
+`
+  <nav class="navbar">
+    <a href="/index.html" class="nav-title">Corvandil Codex</a>
 
-fetch("./data/pantheon.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const output = document.getElementById("pantheon-output");
-
-    // Title
-    const heading = document.createElement("h2");
-    heading.textContent = data.circle;
-
-    // List of deities
-    const list = document.createElement("ul");
-
-    data.deities.forEach((d) => {
-      const item = document.createElement("li");
-      item.textContent = `${d.name} — ${d.title}`;
-      list.appendChild(item);
-    });
-
-    // Render
-    output.innerHTML = "";
-    output.appendChild(heading);
-    output.appendChild(list);
-  })
-  .catch((error) => {
-    console.error("Error loading pantheon:", error);
-  });
+    <div class="nav-links">
+      <a href="/pages/deities.html">Pantheon</a>
+      <a href="#">Locations</a>
+    </div>
+  </nav>
+`
+document.body.insertAdjacentHTML("afterbegin", navbarItems);
